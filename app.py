d1578494d57d83d2be7bfc9e67d01df916c528bd
ThelvynDragon <thelvyndragon@free.fr>
2025-03-23 16:29:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,3 @@ if uploaded_file:
 with open(docx_path, "rb") as f_docx:
     st.download_button("📥 Télécharger le Word", f_docx, "Notion_Document.docx")
 
-    # Aperçu HTML simple du document Word
-    from docx import Document
-    doc = Document(docx_path)
-    st.subheader("Aperçu du contenu Word")
-    for para in doc.paragraphs:
-        st.markdown(f"<p>{para.text}</p>", unsafe_allow_html=True)
-
-
-            shutil.rmtree(work_dir)
